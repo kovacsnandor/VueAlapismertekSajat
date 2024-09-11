aad8d897fc2277c220484cb3059212d23469a6eb
kovacsnandor <kovacsnandor@gmail.com>
2024-09-11 14:10:25 +0200

    vshow majdnem kész

--- src/views/Vshow.vue
+++ src/views/Vshow.vue
@@ -1,15 +1,46 @@
 <template>
   <div>
     <h2>v-show</h2>
+    <div class="alert alert-primary" role="alert" v-show="show1" id="show1">
+            <div class="d-flex justify-content-between">
+            Eltüntetés v-show-val. Ilyenkor csak a láthatóság tűnik el, marad DOM-ban
+            <i class="bi bi-x-lg my-pointer"
+                @click="onClickX1()"
+            ></i>
+        </div>
+    </div>
+    <div class="alert alert-success" role="alert" v-if="show2" id="show2">
+            <div class="d-flex justify-content-between">
+            Eltüntetés v-if-el. Ilyenkor kiveszi DOM-ból
+            <i class="bi bi-x-lg my-pointer"
+                @click="onClickX2()"
+            ></i>
+        </div>
+    </div>
+    <button type="button" class="btn btn-primary">üzenet</button>
+
   </div>
 </template>
 
 <script>
 export default {
-
-}
+    data(){
+        return {
+            show1: true,
+            show2: true,
+            showAutoMessage: null
+        }
+    },
+    methods: {
+        onClickX1(){
+            this.show1=false;
+        },
+        onClickX2(){
+            this.show2=false;
+        }
+    }
+};
 </script>
 
 <style>
-
 </style>
