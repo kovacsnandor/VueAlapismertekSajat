90bdb640b2aca6d28eb6942a416ed4a4718778eb
kovacsnandor <kovacsnandor@gmail.com>
2024-09-16 13:12:30 +0200

    takarítás

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,13 @@
-<script setup>
-import TheWelcome from '../components/TheWelcome.vue'
+<template>
+  <h2>Home</h2>
+</template>
+
+<script>
+export default {
+
+}
 </script>
 
-<template>
-  <main>
-    <TheWelcome />
-  </main>
-</template>
+<style>
+
+</style>
