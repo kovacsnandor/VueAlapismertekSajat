447d17cdddd6917e0733fe4935116d81f861c4f1
kovacsnandor <kovacsnandor@gmail.com>
2024-10-22 09:05:43 +0200

    validáció kész

--- src/components/PersonForm.vue
+++ src/components/PersonForm.vue
@@ -1,6 +1,6 @@
 <template>
   <p>{{ personForm }}</p>
-  <form class="row g-4" @submit.prevent="onClickSubmit()">
+  <form class="row g-4 needs-validation was-validated" novalidate>
     <!-- Name -->
     <div class="col-md-8">
       <label for="name" class="form-label">Név:</label>
@@ -11,6 +11,8 @@
         required
         v-model="personForm.name"
       />
+      <div class="valid-feedback">&nbsp;</div>
+      <div class="invalid-feedback">Név kitöltése kötelező</div>
     </div>
     <!-- dateOfBird -->
     <div class="col-md-4">
@@ -24,7 +26,7 @@
       />
     </div>
     <!-- locality -->
-    <div class="col-md-6">
+    <div class="col-md-6 position-relative">
       <label for="locality" class="form-label">Helység:</label>
       <input
         type="text"
@@ -33,6 +35,7 @@
         required
         v-model="personForm.locality"
       />
+      <div class="invalid-tooltip">Név kitöltése kötelező</div>
     </div>
     <!-- zipCode -->
     <div class="col-md-3">
@@ -84,6 +87,7 @@
         aria-label="Default select example"
         id="profession"
         v-model="personForm.professionId"
+        required
       >
         <option
           :value="profession.id"
@@ -104,12 +108,29 @@ import dateFormat, { masks } from "dateformat";
 export default {
   props: ["personForm", "professions"],
   emits: ["savePerson"],
+  mounted() {
+    const forms = document.querySelectorAll(".needs-validation");
+
+    // Loop over them and prevent submission
+    Array.from(forms).forEach((form) => {
+      form.addEventListener(
+        "submit",
+        (event) => {
+          event.preventDefault();
+          event.stopPropagation();
+          if (form.checkValidity()) {
+            this.onClickSubmit();
+          }
+        },
+        false
+      );
+    });
+  },
   methods: {
-    onClickSubmit(){
-        console.log("Mentés");
-        this.$emit("savePerson",this.personForm)
-        
-    }
+    onClickSubmit() {
+      console.log("Mentés");
+      this.$emit("savePerson", this.personForm);
+    },
   },
   computed: {
     formattedDate: {
@@ -117,15 +138,15 @@ export default {
         if (!this.personForm.dateOfBird) {
           return null;
         }
-        return dateFormat(this.personForm.dateOfBird,'yyyy-mm-dd');
+        return dateFormat(this.personForm.dateOfBird, "yyyy-mm-dd");
 
         // return this.personForm.dateOfBird
       },
       set(newValue) {
-        this.personForm.dateOfBird = dateFormat(newValue,'yyyy.mm.dd');
-      }
-    }
-  }
+        this.personForm.dateOfBird = dateFormat(newValue, "yyyy.mm.dd");
+      },
+    },
+  },
 };
 </script>
 
