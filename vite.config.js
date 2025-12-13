4b4a2bf744539d2ffb7ddea351199b04b87bf702
kovacsnandor <kovacsnandor@gmail.com>
2025-12-13 21:30:45 +0100

    vite.config.js átalakítás

--- vite.config.js
+++ vite.config.js
@@ -1,31 +1,31 @@
 import { fileURLToPath, URL } from 'node:url'
-
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite' // loadEnv importálása
 import vue from '@vitejs/plugin-vue'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-  ],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+// A konfigurációt már nem objektumként, hanem függvényként exportáljuk.
+export default defineConfig(({ mode }) => { 
+    
+    // 1. Környezeti Változók Betöltése
+    // A Vite API-t használjuk a mode és az aktuális mappa alapján.
+    // Csak a 'VITE_' kezdetű változókat töltjük be.
+    const env = loadEnv(mode, process.cwd(), 'VITE_');
+    
+    // 2. Visszatérés a konfigurációs objektummal
+    return {
+        plugins: [
+            vue(),
+        ],
+        resolve: {
+            alias: {
+                '@': fileURLToPath(new URL('./src', import.meta.url))
+            }
+        },
+
+        build: {
+            outDir: env.VITE_BUILD_DIR,
+            emptyOutDir: false,
+        },
+        
+        base: mode === 'development' ? '/' : env.VITE_WEB_DIR,
     }
-  },
-
-  //Ebbe a mappába fordítja a webalkalmazást
-  //ilyenkor az apach szerver: www/proba mappába kell tenni
-  build: {
-    outDir: './dist/proba',
-  },
-
-  //Ez egy belső környezeti változó, ami:
-  //npm run dev hatására: development
-  //npm run build hatására: production
-  //A base: mondja meg, hogy honnan idul a projekt
-  //developent módban: / -> src mappa
-  //product módban: a dist/proba mappa, vagy élesben a szerveren: www/proba (a lefordított alakalmazás)
-  base: process.env.NODE_ENV === 'development' ? '/' : '/proba/',
-
-})
+});
