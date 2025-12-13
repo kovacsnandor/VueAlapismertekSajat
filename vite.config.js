a479a59988a3177f7971f6249666603528d84b92
kovacsnandor <kovacsnandor@gmail.com>
2025-12-13 20:39:37 +0100

    vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -12,5 +12,20 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+
+  //Ebbe a mappába fordítja a webalkalmazást
+  //ilyenkor az apach szerver: www/proba mappába kell tenni
+  build: {
+    outDir: './dist/proba',
+  },
+
+  //Ez egy belső környezeti változó, ami:
+  //npm run dev hatására: development
+  //npm run build hatására: production
+  //A base: mondja meg, hogy honnan idul a projekt
+  //developent módban: / -> src mappa
+  //product módban: a dist/proba mappa, vagy élesben a szerveren: www/proba (a lefordított alakalmazás)
+  base: process.env.NODE_ENV === 'development' ? '/' : '/proba/',
+
 })
